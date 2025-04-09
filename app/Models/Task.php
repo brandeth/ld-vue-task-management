71ef6746e1c217bee6aa6669c514f4e7f21002bd
Rey Garcia <jreygarcia24@gmail.com>
2025-04-09 15:03:49 +0800

    WIP: Added file input in Create page.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Task extends Model
+class Task extends Model implements HasMedia
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = ['name', 'is_completed', 'due_date'];
 
